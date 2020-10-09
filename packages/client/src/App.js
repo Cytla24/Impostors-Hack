137196388fcfe493d23837827d29531c7cd2639d
Collins1738 <tobechikeluba@gmail.com>
2020-10-09 15:40:23 -0400

    cleaned client

---
+++ packages/client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+// import { Link, Switch, Route, withRouter } from 'react-router-dom';
+// import './index.scss';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            Hi bishes
+        </div>
+    );
+  }
+}
+
+export default App;
+// export default withRouter(App);
