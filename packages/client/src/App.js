39959fab22b0e0581e7d0d5ccd31cebab7dc8041
Collins1738 <tobechikeluba@gmail.com>
2020-10-09 22:10:38 -0400

    collins changes lol

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-// import { Link, Switch, Route, withRouter } from 'react-router-dom';
-// import './index.scss';
+import { SearchBar } from "./components/searchbar";
 
 class App extends Component {
 	render() {
-		return <div>Hi bishes</div>;
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
 	}
 }
 
---
+++ packages/client/src/components/card.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Card, CardContent, CardActions, Button } from "@material-ui/core";
+
+const useStyles = (theme) => ({
+	root: {
+		maxWidth: 345,
+	},
+});
+
+class CardInner extends React.Component {
+	renderButtons = () => {
+		const buttons = [<Button key="more">Offset Carbon Footprint</Button>];
+		return buttons;
+	};
+
+	render() {
+		const { classes, origin, destination, mode, time } = this.props;
+		const arrow = "=>";
+		return (
+			<Card className={classes.root}>
+				<CardContent>
+					<h2>
+						{origin}
+						{arrow}
+						{destination}
+					</h2>
+					<h3>By {mode}</h3>
+					<p>{time}</p>
+				</CardContent>
+				<CardActions>{this.renderButtons()}</CardActions>
+			</Card>
+		);
+	}
+}
+
+export const MyCard = withStyles(useStyles)(CardInner);
